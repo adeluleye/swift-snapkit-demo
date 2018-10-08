1f073b75e6c607dac1bbcfc3550a0e82dc4d6433
adeluleye <adelu4real@gmail.com>
2018-10-08 01:39:40 +0100

    Initial Commit

---
+++ SnapKitDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SnapKitDemo
+//
+//  Created by ADELU ABIDEEN ADELEYE on 10/8/18.
+//  Copyright © 2018 Spantom Technologies Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
